93a67ae07467f97566e5c16cbc89b95f7ba05697
titanh3art <18174614+titanh3art@users.noreply.github.com>
2026-01-28 16:18:04 +0530

    prettier and vscode formatting config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   tabWidth: 2,
-  semi: false,
+  semi: true,
   printWidth: 100,
-  trailingComma: "all",
-}
+  trailingComma: "es5",
+  arrowParens: "always",
+  bracketSpacing: true,
+  endOfLine: "lf",
+};
