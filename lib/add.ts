7636f58b386a27f889482b8cdac03dfbe2fb4a18
titanh3art <18174614+titanh3art@users.noreply.github.com>
2026-01-29 16:21:12 +0530

    cds add plugin adds xsuaa scope to xs-security.json

--- lib/add.ts
+++ lib/add.ts
@@ -1,26 +1,16 @@
-import cds from "@sap/cds-dk";
+const cds = require("@sap/cds-dk");
 
-const { path, read, exists } = cds.utils;
-const { join } = path;
+const { exists } = cds.utils;
 
 module.exports = class DataInspectorAddPlugin extends cds.add.Plugin {
-  options() {
-    return {
-      "app-content-name": {
-        type: "string",
-        help: "Given name of your MTA module com.sap.application.content",
-      },
-    };
-  }
-
   async run() {
     /**
-     * Add xsuaa scope to xs-security.json
+     * Add data-inspector scope to xs-security.json
      */
-
-    // hasXsuaa may not be reliable; directly check for the existence of xs-security.json
     if (exists("xs-security.json")) {
-      await cds.add.merge(__dirname, "../templates/xs-security.json.hbs").into("xs-security.json");
+      await cds.add.merge(__dirname, "../templates/xs-security.json.hbs").into("xs-security.json", {
+        additions: [{ in: "scopes", where: { name: "$XSAPPNAME.capDataInspectorReadonly" } }],
+      });
     }
   }
 };
